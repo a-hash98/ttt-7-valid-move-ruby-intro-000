19838a9b16efbea963b022b357edf3d8f7c7c0c6
Ay Has <ahashim983@yahoo.com>
2019-03-14 11:51:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ end
 
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if !board[index] == " " && !board[index] == "" && !board[index] == "nil"
+  if board[index] == "X" || board[index] == "O"
     return true
   else
     return false
