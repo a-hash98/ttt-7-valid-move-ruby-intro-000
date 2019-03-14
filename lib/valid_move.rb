ae1e18621969f1c9d10b52d9e77c45de4fd29c9d
Ay Has <ahashim983@yahoo.com>
2019-03-14 11:42:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,8 @@
 def valid_move?(board,index)
-
   if index.between?(0,8)
-      if !position_taken?
-        return true
-      end
-  elsif position_taken? || !index.between(0,8)
-      return false
-
+    return true
+  else
+    return false
   end
 end
 
