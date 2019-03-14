72d4ba469998850e22db2d93aa2fb6e263f0a995
Ay Has <ahashim983@yahoo.com>
2019-03-14 11:34:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,35 @@
+def valid_move?(board,index)
+
+  if !(index.between?(0,8))
+      return false
+      if position_taken?
+        return fals
+      end
+  elsif index.between?(0,8)
+    if !position_taken
+      return true
+    else
+      return false
+    end
+  end
+end
+
+# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " "|| board[index] == "" || board[index] == "nil"
+    return false
+  else
+    return true
+  end
+#  elsif board[index] == "X"|| board[index] == "O"
+#    return true
+
+end
+
+def convert_to_board_position(user_position)
+  index = user_position.to_i-1
+  return index
+end
+
 # code your #valid_move? method here
-
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
